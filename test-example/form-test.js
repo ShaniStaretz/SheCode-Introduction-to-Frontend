557e20fdf5084eae0cce0a71dc454ffc103a939c
ShaniStaretz <shakimi88@gmail.com>
2026-05-26 19:52:05 +0300

    Delete test-example directory

--- test-example/form-test.js
+++
@@ -1,8 +0,0 @@
-function onDisplayClick() {
-  alert(document.getElementById("occupation").value);
-}
-
-function onAlertClick() {
-  document.getElementById("greeting").value =
-    "Hello " + document.getElementById("name").value;
-}
