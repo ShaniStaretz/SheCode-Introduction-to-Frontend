5e89cfa728a4222f50cc18edf0095891fcbd5f64
Shani Staretz AI <shani.staretz@gmail.com>
2026-05-31 21:13:56 +0300

    fixed question10 in test1

--- tests-example/test1/question10.js
+++ tests-example/test1/question10.js
@@ -1,7 +1,7 @@
-var resultMulti = 0;
-var resultSum = 0;
 function onMultiTwoClick() {
-  resultMulti = document.getElementById("doubleNumber").value * 2;
+  let doubleNumberElement = document.getElementById("doubleNumber");
+  let doubleNumberValue = doubleNumberElement.value;
+  let resultMulti = Number(doubleNumberValue) * 2;
   alert("the result is:" + resultMulti);
 }
 
@@ -10,13 +10,26 @@ function onSumThreeNumbersClick() {
   let num2 = Number(document.getElementById("sum-number2").value);
   let num3 = Number(document.getElementById("sum-number3").value);
 
-  resultSum = num1 + num2 + num3;
+  let resultSum = num1 + num2 + num3;
   alert("the result is: " + resultSum);
 }
 
 function onSubmitClick() {
+  //Get the first name and last name values from the input fields
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
+
+  //Get the number value from the input field and multiply it by two
+  let doubleNumberElement = document.getElementById("doubleNumber");
+  let doubleNumberValue = doubleNumberElement.value;
+  let resultMulti = Number(doubleNumberValue) * 2;
+
+  //Get the three numbers values from the input fields and sum them
+  let num1 = Number(document.getElementById("sum-number1").value);
+  let num2 = Number(document.getElementById("sum-number2").value);
+  let num3 = Number(document.getElementById("sum-number3").value);
+
+  let resultSum = num1 + num2 + num3;
   alert(
     "Good Job" +
       firstName +
